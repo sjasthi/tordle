442b918c94243c49222375e2fef159e9cece573e
ping58972 <ping58972@gmail.com>
2022-02-18 18:33:32 -0600

    fixed letter to be color and removed keyboad

--- static/js/main.js
+++ static/js/main.js
@@ -1,161 +1,187 @@
-document.addEventListener("DOMContentLoaded", () => {
-  createSquares();
-  getNewWord();
+//console.log('keypress');
+const lettersPattern = /[a-z]/; // /^[A-Za-z][A-Za-z0-9]*$/;
+let currentGuessCount = 1;
+let currentGuess = document.querySelector('#guess' + currentGuessCount);
+let words = ['apple', 'baker', 'store', 'horse', 'speak', 'clone', 'bread'];
+let solutionWord = '';
 
-  let guessedWords = [[]];
-  let availableSpace = 1;
+const chooseWord = () => {
+    // choose random item from words array
+    let randomItem = Math.floor(Math.random() * (words.length - 1)) + 1;
+    solutionWord = words[randomItem];
+};
 
-  let word;
-  let guessedWordCount = 0;
+chooseWord();
+console.log('solution word = ' + solutionWord);
 
-  const keys = document.querySelectorAll(".keyboard-row button");
-
-  function getNewWord() {
-    fetch(
-      `https://wordsapiv1.p.rapidapi.com/words/?random=true&lettersMin=5&lettersMax=5`,
-      {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-          "x-rapidapi-key": "<YOUR_KEY_GOES_HERE>",
-        },
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((res) => {
-        word = res.word;
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
-
-  function getCurrentWordArr() {
-    const numberOfGuessedWords = guessedWords.length;
-    return guessedWords[numberOfGuessedWords - 1];
-  }
-
-  function updateGuessedWords(letter) {
-    const currentWordArr = getCurrentWordArr();
-
-    if (currentWordArr && currentWordArr.length < 5) {
-      currentWordArr.push(letter);
-
-      const availableSpaceEl = document.getElementById(String(availableSpace));
-
-      availableSpace = availableSpace + 1;
-      availableSpaceEl.textContent = letter;
+// detect keypress (letter, backspace, enter, other)
+document.addEventListener('keydown', (e) => {
+    //console.log('keypress: ' + e.key);
+    let keypress = e.key;
+    if (currentGuessCount < 7) {
+        if (
+            keypress.length == 1 &&
+            lettersPattern.test(e.key) &&
+            currentGuess.dataset.letters.length < 5
+        ) {
+            //console.log('is letter');
+            updateLetters(keypress);
+        } else if (e.key == 'Backspace' && currentGuess.dataset.letters != '') {
+            //console.log('is backspace');
+            deleteFromLetters();
+        } else if (e.key == 'Enter' && currentGuess.dataset.letters.length == 5) {
+            submitGuess();
+        }
     }
-  }
-
-  function getTileColor(letter, index) {
-    const isCorrectLetter = word.includes(letter);
-
-    if (!isCorrectLetter) {
-      return "rgb(58, 58, 60)";
-    }
-
-    const letterInThatPosition = word.charAt(index);
-    const isCorrectPosition = letter === letterInThatPosition;
-
-    if (isCorrectPosition) {
-      return "rgb(83, 141, 78)";
-    }
-
-    return "rgb(181, 159, 59)";
-  }
-//
-//  function handleSubmitWord() {
-//    const currentWordArr = getCurrentWordArr();
-//    if (currentWordArr.length !== 5) {
-//      window.alert("Word must be 5 letters");
-//    }
-//
-//    const currentWord = currentWordArr.join("");
-//
-//    fetch(`https://wordsapiv1.p.rapidapi.com/words/${currentWord}`, {
-//      method: "GET",
-//      headers: {
-//        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-//        "x-rapidapi-key": "61c5e3986dmsh20c1bee95c2230dp18d1efjsn4668bbcfc1b3",
-//      },
-//    })
-//      .then((res) => {
-//        if (!res.ok) {
-//          throw Error();
-//        }
-//
-//        const firstLetterId = guessedWordCount * 5 + 1;
-//        const interval = 200;
-//        currentWordArr.forEach((letter, index) => {
-//          setTimeout(() => {
-//            const tileColor = getTileColor(letter, index);
-//
-//            const letterId = firstLetterId + index;
-//            const letterEl = document.getElementById(letterId);
-//            letterEl.classList.add("animate__flipInX");
-//            letterEl.style = `background-color:${tileColor};border-color:${tileColor}`;
-//          }, interval * index);
-//        });
-//
-//        guessedWordCount += 1;
-//
-//        if (currentWord === word) {
-//          window.alert("Congratulations!");
-//        }
-//
-//        if (guessedWords.length === 6) {
-//          window.alert(`Sorry, you have no more guesses! The word is ${word}.`);
-//        }
-//
-//        guessedWords.push([]);
-//      })
-//      .catch(() => {
-//        window.alert("Word is not recognised!");
-//      });
-//  }
-
-  function createSquares() {
-    const gameBoard = document.getElementById("board");
-
-    for (let index = 0; index < 30; index++) {
-      let square = document.createElement("div");
-      square.classList.add("square");
-      square.classList.add("animate__animated");
-      square.setAttribute("id", index + 1);
-      gameBoard.appendChild(square);
-    }
-  }
-
-//  function handleDeleteLetter() {
-//    const currentWordArr = getCurrentWordArr();
-//    const removedLetter = currentWordArr.pop();
-//
-//    guessedWords[guessedWords.length - 1] = currentWordArr;
-//
-//    const lastLetterEl = document.getElementById(String(availableSpace - 1));
-//
-//    lastLetterEl.textContent = "";
-//    availableSpace = availableSpace - 1;
-//  }
-
-  for (let i = 0; i < keys.length; i++) {
-    keys[i].onclick = ({ target }) => {
-      const letter = target.getAttribute("data-key");
-
-      if (letter === "enter") {
-        handleSubmitWord();
-        return;
-      }
-
-      if (letter === "del") {
-        handleDeleteLetter();
-        return;
-      }
-
-      updateGuessedWords(letter);
-    };
-  }
 });
+
+const submitGuess = () => {
+    //console.log('submit guess');
+    for (let i = 0; i < 5; i++) {
+        setTimeout(() => {
+            revealTile(i, checkLetter(i));
+        }, i * 200);
+    }
+};
+
+const checkIfGuessComplete = (i) => {
+    if (i == 4) {
+        checkWin();
+    }
+};
+
+const jumpTiles = () => {
+    //console.log('jumpTiles');
+    //console.log(currentGuessCount);
+    for (let i = 0; i < 5; i++) {
+        setTimeout(() => {
+            let currentTile = document.querySelector(
+                '#guess' + currentGuessCount + 'Tile' + (i + 1)
+            );
+            currentTile.classList.add('jump');
+        }, i * 200);
+    }
+};
+
+const checkWin = () => {
+    //console.log('check win');
+    if (solutionWord == currentGuess.dataset.letters) {
+        // Win
+        //console.log('game is won!');
+        setTimeout(() => {
+            jumpTiles();
+        }, 500);
+    } else {
+        // Not won
+        currentGuessCount = currentGuessCount + 1;
+        currentGuess = document.querySelector('#guess' + currentGuessCount);
+        //console.log('not a win, increment guess count to ' + currentGuessCount);
+        if (currentGuessCount == 7) {
+            setTimeout(() => {
+                showSolution();
+            }, 500);
+        }
+    }
+};
+
+const showSolution = () => {
+    alert('Better luck next time. The solution was: ' + solutionWord);
+};
+
+// Update "letters"
+const updateLetters = (letter) => {
+    let oldLetters = currentGuess.dataset.letters;
+    let newLetters = oldLetters + letter;
+    let currentTile = newLetters.length;
+    currentGuess.dataset.letters = newLetters;
+    //console.log('currentTile = ' + currentTile);
+    updateTiles(currentTile, letter);
+};
+
+// Update tile markup
+const updateTiles = (tileNumber, letter) => {
+    //console.log('updateTiles(' + tileNumber, letter + ')');
+    let currentTile = document.querySelector(
+        '#guess' + currentGuessCount + 'Tile' + tileNumber
+    );
+    currentTile.innerText = letter;
+    currentTile.classList.add('has-letter');
+};
+
+// Backspace -- Delete last letter
+const deleteFromLetters = () => {
+    // remove last letter from data-letters
+    let oldLetters = currentGuess.dataset.letters;
+    //console.log('oldLetters = ' + oldLetters);
+    let newLetters = oldLetters.slice(0, -1);
+    //console.log('newLetters = ' + newLetters);
+    currentGuess.dataset.letters = newLetters;
+    deleteFromTiles(oldLetters.length);
+};
+
+// Backspace -- Delete last tile markup
+const deleteFromTiles = (tileNumber) => {
+    // remove markup from last tile
+    //console.log('deleteFromTiles = ' + tileNumber);
+    let currentTile = document.querySelector(
+        '#guess' + currentGuessCount + 'Tile' + tileNumber
+    );
+    currentTile.innerText = '';
+    currentTile.classList.remove('has-letter');
+};
+
+// Check letter to solution
+// parameter = letter position in word
+const checkLetter = (position) => {
+    //console.log('checkLetter');
+    let guessedLetter = currentGuess.dataset.letters.charAt(position);
+    let solutionLetter = solutionWord.charAt(position);
+    //console.log(guessedLetter, solutionLetter);
+
+    // If letters match, return "correct"
+    if (guessedLetter == solutionLetter) {
+        return 'correct';
+    }
+    // If not a match, if letter exists in solution word, return "present"
+    else {
+        return checkLetterExists(guessedLetter) ? 'present' : 'absent';
+    }
+
+    // If not a match, if letter doesn't exist in solution, return "absent"
+};
+
+const checkLetterExists = (letter) => {
+    return solutionWord.includes(letter);
+};
+
+const revealTile = (i, state) => {
+    //console.log('revealTile = ' + i, state);
+    let tileNum = i + 1;
+    flipTile(tileNum, state);
+    checkIfGuessComplete(i);
+};
+
+const flipTile = (tileNum, state) => {
+    let tile = document.querySelector(
+        '#guess' + currentGuessCount + 'Tile' + tileNum
+    );
+    tile.classList.add('flip-in');
+    setTimeout(() => {
+        tile.classList.add(state);
+    }, 250);
+    setTimeout(() => {
+        tile.classList.remove('flip-in');
+        tile.classList.add('flip-out');
+    }, 250);
+    setTimeout(() => {
+        tile.classList.remove('flip-out');
+    }, 1500);
+};
+/*
+- if keypress is a letter
+  - update "letters" attribute
+    - update tile markup based on "letters" value
+- if keypress is backspace
+  - delete last letter in "letters"
+    - update tile markup based on "letters"
+*/
