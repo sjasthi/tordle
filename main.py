e4f8ef052a52f622bc7674a2437559929881a366
ping58972 <ping58972@gmail.com>
2022-03-01 23:14:50 -0600

    processing on Telugu

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
 from flask import Flask, render_template, url_for, redirect, request
 from utility.helper import *
 
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
 l = ["0", "1", "2", "add", "font", "clone", "telugu", "english", "absolute"]
+telugus = ["సిరివెన్నెల"]
+guess_ts = [
+    "తమిళభాష",
+    "సిరివెన్నెల",
+    "సరేవీనీలా",
+    "వానలసిరి",
+    "వెన్నెలకాడు",
+    "వెన్నెలరేడు",
+]
+
 params = {"length": 5, "attempt": 6, "language": "english"}
 words = {"solution": l[5], "guess": ""}
+
+# tt = getCharsFromAPI(telugus[0], "Telugu")
+# print(tt)
+# test = getCharsFromAPI(guess_ts[3], "Telugu")
+# print(test)
+# result = ""
+# if tt[1] == test[1]:
+#     for i in range(tt[1]):
+#         if tt[0][i] == test[0][i]:
+#             result += "1"
+#         else:
+#             result += "0"
+# print(result)
+# # utf8 = chr(38)
+# # ns = utf8.encode("utf8")
+# bi = "రి"[0]
+# # bi = r"{}".format(bii)
+# print(bi)
+# print("\u0c38\u0c3f\u0c30\u0c3f\u0c35")
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    words["solution"] = l[int(request.args.get("length") or params["length"])]
-    update_data(params, request, words)
+    if request.form:
+        print(request.form)
+        if request.form["language"] == "english":
+            words["solution"] = l[int(request.form["length"] or params["length"])]
+            update_data(params, request, words)
+        else:
+            pass
     words["solution"] = l[params["length"]]
     print(params)
     print(words)
     return render_template("index.html", words=words, params=params)
-
-
-# @app.route("/english", methods=["GET", "POST"])
-# def english():
-#     update_data(params, request, words)
-#     words["solution"] = l[params["length"]]
-#     print(words)
-#     return render_template("index.html", words=words, params=params)
-
-
-# @app.route("/telugu", methods=["GET", "POST"])
-# def telugu():
-#     words["solution"] = l[int(request.args.get("length") or params["length"])]
-#     update_data(params, request, words, lang="telugu")
-#     print(words)
-#     return render_template("index.html", words=words, params=params)
 
 
 if __name__ == "__main__":
--- utility/helper.py
+++ utility/helper.py
@@ -1,5 +1,39 @@
+"""Author: Nalongsone Danddank, Assignment #4, Feb 2 2022, ICS499 METRO STATE """
+
+import requests
+import json
+
+
 def update_data(params, request, words, lang="english"):
     params["length"] = int(request.form["length"] or params["length"])
     params["attempt"] = int(request.form["attempt"] or params["attempt"])
     params["language"] = (request.form["language"]).lower() or params["language"]
     words["guess"] = request.form.get("word") or words["guess"]
+
+
+def getCharsFromAPI(string, language):
+    """@string: String for api params
+    @language: String for api params"""
+    URL_char = "https://indic-wp.thisisjava.com/api/getLogicalChars.php"
+    URL_len = "https://indic-wp.thisisjava.com/api/getLength.php"
+    params = {"string": string, "language": language}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+    }
+    # get text or string that request from API
+    r_chars = requests.get(url=URL_char, params=params, headers=headers, timeout=5)
+    # handle text like json format from api by encode and decode the string.
+    text = r_chars.text
+    if text.startswith("\ufeff"):
+        text = text.encode("utf8")[6:].decode("utf8")
+    # convert text to dictionary by json
+    data = json.loads(text)
+
+    # get chars length from API
+    r_len = requests.get(url=URL_len, params=params, headers=headers, timeout=5)
+    text = r_len.text
+    if text.startswith("\ufeff"):
+        text = text.encode("utf8")[6:].decode("utf8")
+    len_json = json.loads(text)
+    # return both chars list and its length
+    return data["data"], len_json["data"]
