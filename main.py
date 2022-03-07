1a156192dd05954bcd93968994268500d7f9025e
ping58972 <ping58972@gmail.com>
2022-03-07 04:23:37 -0600

    Finished Telugu and processing on database SQLite

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 from flask import Flask, render_template, url_for, redirect, request
 from utility.helper import *
 
-
+l = ["0", "1", "2", "add", "font", "clone", "telugu", "english", "absolute"]
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
-l = ["0", "1", "2", "add", "font", "clone", "telugu", "english", "absolute"]
-telugus = ["సిరివెన్నెల"]
-guess_ts = [
-    "తమిళభాష",
-    "సిరివెన్నెల",
-    "వెన్నెలరేడు",
-    "వెన్నెలకాడు",
-    "సరేవీనీలా",
-    "వానలసిరి",
-]
-
 params = {"length": 5, "attempt": 6, "language": "English"}
 words = {
-    "solution": l[5],
+    "solution": l[params["length"]],
     "result": [],
     "word_input": "",
     "wordCount": 0,
-    "word_len_test": False,
+    "word_len_test": True,
+    "status": "PROCESS",
 }
 
 
@@ -29,17 +19,10 @@ words = {
 def home():
     if request.form:
         if len(request.form) > 1:
-            if request.form["language"] == "English":
-                words["solution"] = l[int(request.form["length"] or params["length"])]
-                update_data(params, request, words)
-            elif request.form["language"] == "Telugu":
-                update_data(params, request, words)
-                words["solution"] = getCharsFromAPI(telugus[0], "Telugu")
+            update_data(params, request, words)
         elif len(request.form) == 1 and params["language"] == "Telugu":
-            if request.form["word"] != "":
-                words["word_input"] = request.form["word"]
-                getResult(words)
-
+            words["word_input"] = request.form["word"]
+            getResult(words)
     print(words)
     print(params)
     return render_template("index.html", words=words, params=params)
--- utility/helper.py
+++ utility/helper.py
@@ -4,16 +4,42 @@ import requests
 import json
 
 
+l = ["0", "1", "2", "add", "font", "clone", "telugu", "english", "absolute"]
+telugus = [
+    "0",
+    "1",
+    "2",
+    "సిరివె",
+    "సిరివెన్నె",
+    "సిరివెన్నెల",
+    "సిరివెన్నెలరి",
+    "సిరివెన్నెలరివె",
+    "సిరివెన్నెలరివెత",
+]
+guess_ts = [
+    "తమిళభాష",
+    "సిరివెన్నెల",
+    "వెన్నెలరేడు",
+    "వెన్నెలకాడు",
+    "సరేవీనీలా",
+    "వానలసిరి",
+]
+
+
 def update_data(params, request, words):
     params["length"] = int(request.form["length"] or params["length"])
     params["attempt"] = int(request.form["attempt"] or params["attempt"])
-    params["language"] = (request.form["language"]) or params["language"]
-    words["result"] = []
-    words["word_input"] = ""
-    words["wordCount"] = 0
-
-
-# words = {"solution": l[5], "result": [], "word_input": "", "wordCount": 0}
+    if request.form["language"] == "English":
+        params["language"] = "English"
+        words["solution"] = l[params["length"]]
+    elif request.form["language"] == "Telugu":
+        params["language"] = "Telugu"
+        words["result"] = []
+        words["word_input"] = ""
+        words["wordCount"] = 0
+        words["status"] = "PROCESS"
+        words["word_len_test"] = True
+        words["solution"] = getCharsFromAPI(telugus[params["length"]], "Telugu")
 
 
 def getCharsFromAPI(string, language):
@@ -25,7 +51,7 @@ def getCharsFromAPI(string, language):
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
     }
     # get text or string that request from API
-    r_chars = requests.get(url=URL_char, params=params, headers=headers, timeout=5)
+    r_chars = requests.get(url=URL_char, params=params, headers=headers, timeout=50)
     # handle text like json format from api by encode and decode the string.
     text = r_chars.text
     if text.startswith("\ufeff"):
@@ -42,6 +68,9 @@ STATUS = ["PROCESS", "SUCCESS", "END"]
 def getResult(words):
     solution_tlg = words["solution"]
     n = len(solution_tlg)
+    if words["word_input"] == "":
+        words["word_len_test"] = False
+        return
     guess_tlg = getCharsFromAPI(words["word_input"], "Telugu")
     if n == len(guess_tlg):
         words["word_len_test"] = True
@@ -71,19 +100,18 @@ def getResult(words):
                 {
                     "color_arr": result,
                     "tlg_arr": guess_tlg,
-                    "status": STATUS[1],
                 }
             )
+            words["status"] = STATUS[1]
             words["wordCount"] += 1
         else:
             words["result"].append(
                 {
                     "color_arr": result,
                     "tlg_arr": guess_tlg,
-                    "status": STATUS[0],
                 }
             )
+            words["status"] = STATUS[0]
             words["wordCount"] += 1
     else:
         words["word_len_test"] = False
-        # words["result"].append([guess_tlg, result])
